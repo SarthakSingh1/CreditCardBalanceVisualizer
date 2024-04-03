2fee72c8eda22cdbab52373b9db8eef24f6179c4
sarthaksingh94 <singh94@illinois.edu>
2024-04-03 17:05:14 -0500

    better folder management

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 app.use(express.json()); 
 
